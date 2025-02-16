732bb1fe83dcd5ff948cafa0d4068ae5c977a0bf
1ris_W <ediath462@gmail.com>
2025-02-16 16:51:11 +0800

    Initial Commit

---
+++ customPipDemo/customPipDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  customPipDemoApp.swift
+//  customPipDemo
+//
+//  Created by Iris on 2025-02-16.
+//
+
+import SwiftUI
+
+@main
+struct customPipDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
